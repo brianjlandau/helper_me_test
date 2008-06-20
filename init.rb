7d7b8a5e8044d18ce6c6b8e644718147866e69b1
Brian Landau <brianjlandau@gmail.com>
2008-06-19 21:48:13 -0400

    Add assertions code. remove unneeded rake task file

--- init.rb
+++ init.rb
@@ -1,1 +1,8 @@
-# Include hook code here
+require 'helper_me_test'
+
+ActionView::TestCase.send :include, HelperMeTest.TagAssertions
+ActionView::TestCase.send :include, HelperMeTest.SelectorAssertions
+if defined? Hpricot
+  ActionView::TestCase.send :include, HelperMeTest.HpricotAssertions
+end
+
--- tasks/helper_me_test_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :helper_me_test do
-#   # Task goes here
-# end
